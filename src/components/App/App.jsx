35c11d9f37d790ff989388fded636bf0fa1a6768
Emily Schwartau <emilyschwartau+1@gmail.com>
2021-10-22 16:37:24 -0500

    initial commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+//import React from 'react';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import axios from 'axios';
+import GalleryList from "../GalleryList/GalleryList";
+
 
 function App() {
+
+let [galleryList, setGalleryList] = useState([]);
+
+//GET photos from array
+const getGalleryList = () => {
+  axios
+    .get("/gallery")
+    .then((response) => {
+      console.log("Gallery list ->", response.data);
+      setGalleryList(response.data);
+    })
+    .catch((err) => {
+      console.log("Error getting groceries", err);
+    });
+};
+
+useEffect(() => {
+  getGalleryList();
+}, []);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -9,6 +33,7 @@ function App() {
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+        <img src='images/red_wing.jpg'/>
       </div>
     );
 }
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,18 @@
+
+function GalleryList({galleryList, getGalleryList}) {
+    console.log('in GalleryList');
+
+    return (
+        <>
+            {galleryList.map((item) => (
+              <GalleryItem
+                item={item}
+                // need??? key={item.id}
+                getGalleryList={getGalleryList}
+              />
+            ))}
+        </>
+      );
+}
+
+export default GalleryList
