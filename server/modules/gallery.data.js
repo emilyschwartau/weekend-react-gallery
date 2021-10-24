c4139d9ce34301977a79da0f22a2fd59026ea42a
Emily Schwartau <emilyschwartau+1@gmail.com>
2021-10-23 21:17:55 -0500

    conditional rendering to show photo or description

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,10 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/emily_and_emma.jpg', description: 'Picture of Emily and her friend Emma at Guanella Pass, CO', likes: 0 },
-    { id: 3, path: 'images/monarch.jpg', description: 'Photo of monarch butterfly', likes: 0 },
-    { id: 4, path: 'images/oregon_trail_ruts.jpg', description: 'Ruts from the oregon trail that are still visible on California Hill', likes: 0 },
-    { id: 5, path: 'images/queen_annes_lace.JPG', description: 'Queen Annes Lace flower along a country road', likes: 0 },
-    { id: 6, path: 'images/red_wing.jpg', description: 'Photo of Red Wing MN taken from the Mississippi River', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0, photoView: true },
+    { id: 2, path: 'images/emily_and_emma.jpg', description: 'Picture of Emily and her friend Emma at Guanella Pass, CO', likes: 0, photoView: false },
+    { id: 3, path: 'images/monarch.jpg', description: 'Photo of monarch butterfly', likes: 0, photoView: true },
+    { id: 4, path: 'images/oregon_trail_ruts.jpg', description: 'Ruts from the oregon trail that are still visible on California Hill', likes: 0, photoView: false },
+    { id: 5, path: 'images/queen_annes_lace.JPG', description: 'Queen Annes Lace flower along a country road', likes: 0, photoView: true },
+    { id: 6, path: 'images/red_wing.jpg', description: 'Photo of Red Wing MN taken from the Mississippi River', likes: 0, photoView: false }
 ];
 
 module.exports = galleryItems;
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,18 +1,43 @@
 import './GalleryItem.css'
+import axios from 'axios';
 
 function GalleryItem ({ item, getGalleryList }) {
 
-    return (
-        <div className="gallery-item">
-          {/* need??? <p>{item.item}</p> */}
-          <p><img src={item.path}/></p>
-          {/* <p>{item.description}</p> */}
-          <p>{item.likes} people love this!</p>
-           
-        </div>
-      );
+    let returnItem = '';
 
+    if (item.photoView == true) { 
+        returnItem = 
+        (<div className="gallery-item">
+            <p><img src={item.path}/></p>
+            <p>{item.likes} people love this!</p>
+        </div>)  
+    }//end if
 
+    else if (item.photoView == false) {
+        returnItem = 
+        (<div className="gallery-item">
+            <p>{item.description}</p>
+            <p>{item.likes} people love this!</p>
+        </div>)
+    }//end else if 
+
+    const ChangePhotoViewToTrue = () => {
+        //let idToPurchase = item.id;
+        console.log("Clicked ChangePhotoViewToTrue", item.id);
+        axios({
+          method: "PUT",
+          url: '/gallery/like/:id'
+        })
+          .then((response) => {
+            console.log("PUT purchase response is", response);
+            getGroceryList();
+          })
+          .catch((error) => {
+            console.log("PUT purchase error", error);
+          });
+      };
+
+    return returnItem;
 }
 
 export default GalleryItem;
